09a53d7a7de80845fbc103c2635237c0540fefec
Da-Peng <wu526515597@hotmail.com>
2025-04-28 17:25:49 +0800

    调整

--- main.py
+++ main.py
@@ -270,12 +270,22 @@ with gr.Blocks(title="GT") as app:
         gr.Markdown("### 任务与图片")
         gr.Markdown("选择任务文件夹以查看热词文件夹及对应图片。")
         with gr.Row():
-            with gr.Column():
-                task_folders = gr.Dropdown(label="任务文件夹", multiselect=False, choices=[''] + get_task_folders(),
-                                           allow_custom_value=True)
+            task_folders = gr.Dropdown(label="任务文件夹", multiselect=False, choices=[''] + get_task_folders(),
+                                       allow_custom_value=True)
 
-                hotword_folders = gr.Dropdown(label="热词文件夹", multiselect=False,
-                                              allow_custom_value=True)
+            hotword_folders = gr.Dropdown(label="热词文件夹", multiselect=False,
+                                          allow_custom_value=True)
+        refresh_button = gr.Button("刷新任务文件夹")  # 新增刷新按钮
+
+
+        def update_drop_down():
+            return gr.Dropdown(label="任务文件夹", multiselect=False, choices=get_task_folders(),
+                               allow_custom_value=True)
+
+
+        refresh_button.click(update_drop_down, outputs=task_folders)
+        with gr.Row():
+            with gr.Column():
                 research_button = gr.Button("🤐指定热词深度搜索")
                 def research_hot_word(hot_words_folders_path):
                     agent_log_file_path = f"agent_{datetime.datetime.now().strftime('%Y年%m月%d日%H时%M分')}.log"
@@ -286,26 +296,8 @@ with gr.Blocks(title="GT") as app:
 
                     return ret
                 research_button.click(fn=research_hot_word, inputs=[hotword_folders],
-                                      outputs=gr.Textbox(label="执行结果"))
+                                      outputs=gr.Textbox(label=""))
 
-                agent_log_textbox = gr.Textbox(label="AI Agent执行日志", value=update_agent_log_textbox, lines=10,
-                                               every=5)
-
-
-
-
-
-
-            with gr.Column():
-                refresh_button = gr.Button("刷新任务文件夹")  # 新增刷新按钮
-
-
-                def update_drop_down():
-                    return gr.Dropdown(label="任务文件夹", multiselect=False, choices=get_task_folders(),
-                                       allow_custom_value=True)
-
-
-                refresh_button.click(update_drop_down, outputs=task_folders)
                 research_all_keyword_button = gr.Button("🤐全量热词深度搜索")
 
 
@@ -329,9 +321,12 @@ with gr.Blocks(title="GT") as app:
 
 
                 research_all_keyword_button.click(fn=research_all_hot_word, inputs=[task_folders],
-                                                 outputs=gr.Textbox(label="执行结果"))
+                                                  outputs=gr.Textbox(label=""))
+        with gr.Row():
 
-                image_gallery = gr.Gallery(label="图片", value=[], interactive=False, columns=4)
+            agent_log_textbox = gr.Textbox(label="AI Agent执行日志", value=update_agent_log_textbox, lines=9,
+                                           every=5)
+            image_gallery = gr.Gallery(label="图片", value=[], interactive=False, columns=5)
 
         # 修改回调函数，正确更新 hotword_folders 的选项
         task_folders.change(fn=update_hot_word_folders, inputs=task_folders, outputs=hotword_folders)
