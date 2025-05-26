b8b40a3f3afbee20c4bbd472bc66b25e88b4c382
Da-Peng <wu526515597@hotmail.com>
2025-05-26 13:45:37 +0800

    fix

--- webui/utils/transcribe.py
+++ webui/utils/transcribe.py
@@ -1,27 +1,25 @@
 # stt.py
+import os
+
 from faster_whisper import WhisperModel
 
-<<<<<<< HEAD
 from webui.utils.constant import root_dir
 import warnings
 # 定义模型存放位置
 
 LOCAL_MODEL_PATH=os.path.join(root_dir,'models', "faster-distil-whisper-large-v3.5")
 
-=======
->>>>>>> parent of 4562e4d (fix)
+
 class WhisperModelSingleton:
     _instance = None
     _model = None
 
-<<<<<<< HEAD
+
     def __new__(cls, model_size="deepdml/faster-whisper-large-v3.5", device="auto", compute_type="float16"):
 
         warnings.filterwarnings("ignore", category=FutureWarning)
         warnings.filterwarnings("ignore", category=UserWarning)
-=======
-    def __new__(cls, model_size="large-v3", device="cpu", compute_type="int8"):
->>>>>>> parent of 4562e4d (fix)
+
         if cls._instance is None:
             cls._instance = super(WhisperModelSingleton, cls).__new__(cls)
             # 只加载一次模型
